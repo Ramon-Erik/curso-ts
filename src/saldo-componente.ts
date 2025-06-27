99d04813a27c29d1c5731c51475aa1522bbb467a
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-06-27 18:55:59 -0300

    formatando data e dinheiro

--- src/saldo-componente.ts
+++ src/saldo-componente.ts
@@ -1,6 +1,18 @@
-let saldo = 3000;
+let saldo: number = 3000;
 
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
+const elementoData = document.querySelector(".block-saldo time") as HTMLElement;
+
 if (elementoSaldo != null) {
-    elementoSaldo.textContent = saldo.toString();
+    elementoSaldo.textContent = saldo.toLocaleString("pt-br", { currency: "BRL", style: "currency"});
 } 
+
+if (elementoData != null) {
+    const dataAtual: Date = new Date();
+    elementoSaldo.textContent = dataAtual.toLocaleDateString("pt-br", {
+        weekday: "long",
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+    });
+}
