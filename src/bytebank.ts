2da868160da3dd39a158a84c70fdd692ad662ee0
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-06-27 13:43:49 -0300

    arquivos ts

--- src/bytebank.ts
+++ src/bytebank.ts
@@ -1,48 +1,43 @@
-let saldo = -666
-const elementoValorSaldo = document.querySelector(".saldo-valor span.valor")
-const elementoFormulario = document.querySelector(".block-nova-transacao form")
+let saldo = 3000;
 
-elementoValorSaldo.textContent = saldo
+const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
+if (elementoSaldo != null) {
+    elementoSaldo.textContent = saldo.toString();
+}
 
-
-elementoFormulario.addEventListener("submit", (event) => {
-    event.preventDefault()
+const elementoFormulario = document.querySelector(".block-nova-transacao form") as HTMLFormElement;
+elementoFormulario.addEventListener("submit", function(event) {
+    event.preventDefault();
     if (!elementoFormulario.checkValidity()) {
-        alert("Preencha todos os campos")
-        return
+        alert("Por favor, preencha todos os campos da transação!");
+        return;
     }
 
-    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao")
-    const inputValorTransacao = elementoFormulario.querySelector("#valor")
-    const inputDataTransacao = elementoFormulario.querySelector("#data")
+    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao") as HTMLSelectElement;
+    const inputValor = elementoFormulario.querySelector("#valor") as HTMLInputElement;
+    const inputData = elementoFormulario.querySelector("#data") as HTMLInputElement;
 
-    let tipoTransacao = inputTipoTransacao.value
-    let valor = inputValorTransacao.value
-    let data = inputDataTransacao.value
+    let tipoTransacao: string = inputTipoTransacao.value;
+    let valor: number = inputValor.valueAsNumber;
+    let data: Date = new Date(inputData.value);
 
-    switch (tipoTransacao) {
-        case "Depósito":
-            saldo += valor
-elementoValorSaldo.textContent = saldo
-
-            break;
-        case "Transferência":
-        case "Pagamento de Boleto":
-            saldo -= valor
-elementoValorSaldo.textContent = saldo
-
-            break;
-        default:
-            alert("Pagamento inválido")
-            return
+    if (tipoTransacao == "Depósito") {
+        saldo += valor;
+    } else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
+        saldo -= valor;
+    } else {
+        alert("Tipo de Transação é inválido!");
+        return;
     }
+
+    elementoSaldo.textContent = saldo.toString();
 
     const novaTransacao = {
-        tipoTransacao,
-        valor,
-        data
-    }
-    console.log(novaTransacao)
-    elementoFormulario.reset()
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data
+    };
 
-})
+    console.log(novaTransacao);
+    elementoFormulario.reset();
+});
